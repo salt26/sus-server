89cda2cd45db4cdcfc55d8af4c72cf90a8f6de45
Dantae An <48adt@naver.com>
2019-10-01 19:58:03 +0900

    deployment with result page

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 # How to start
+
+## Development mode
+
 <pre><code>venv\Scripts\activate
 set FLASK_APP=sus.py
 flask run
+</code></pre>
+
+## Deployment mode
+
+<pre><code>venv\Scripts\activate
+set PYTHONPATH=.
+twistd web --port tcp:8000 --wsgi sus.app
 </code></pre>
--- sus.py
+++ sus.py
@@ -1,7 +1,9 @@
 from flask import Flask, url_for, redirect, request, render_template, flash, session
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 app = Flask(__name__)
 app.secret_key = b'\xccM6\x16\xbfVw>b\xbd\xde{\xd4\x01\x83\x90'
+#app.wsgi_app = ProxyFix(app.wsgi_app)
 
 all_responses = {}
 sum_of_scores = 0.0
@@ -67,15 +69,36 @@ def questionnaire():
                 ', Avg. of scores: ' + str(sum_of_scores / response_count))
 
             flash('Thank you! Your response has been recorded.')
-            return redirect(url_for('index'))
+            return redirect(url_for('result'))
         else:
             error = 'Please respond to all 10 questions before submit.'
             return render_template('questionnaire_sus.html', error=error, responses=responses, \
                 respond_already=(session['username'] in all_responses))
 
-    return render_template('questionnaire_sus.html', \
-        respond_already=(session['username'] in all_responses))
+    if session['username'] in all_responses:
+        return render_template('questionnaire_sus.html', \
+            responses=all_responses[session['username']], \
+            respond_already=(session['username'] in all_responses))
+    else:
+        return render_template('questionnaire_sus.html', \
+            respond_already=(session['username'] in all_responses))
 
+@app.route('/result')
+def result():
+    if request.method == 'GET' and ('username' not in session\
+        or session['username'] == None):
+        return redirect(url_for('login'))
+
+    if len(all_responses) > 0:
+        return render_template('result_sus.html', username=session['username'], \
+            respond_already=(session['username'] in all_responses), \
+            response_count=str(len(all_responses)), \
+            average=str(sum_of_scores / len(all_responses)))
+    else:
+        return render_template('result_sus.html', username=session['username'], \
+            respond_already=(session['username'] in all_responses), \
+            response_count='0', \
+            average='0')
 
 @app.errorhandler(404)
 def page_not_found(error):
